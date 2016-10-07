f4493d983ead2d219f6d0771560c79838312186b
rayrase <rmartinez2175@eagle.fgcu.edu>
2016-10-07 14:35:57 -0400

    improved gui, connected controllers to fxml

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("app/views/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/app/views/main.fxml"));
         primaryStage.setTitle("AutoSales");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
--- src/app/controllers/MainController.java
+++ src/app/controllers/MainController.java
@@ -1,7 +1,42 @@
 package app.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import java.io.IOException;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.event.Event;
+
 /**
  * Created by RayDeveloper on 10/7/16.
  */
 public class MainController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    public void initialize(){
+
+    }
+
+    public void login(Event event){
+
+        try{
+
+            Parent root = FXMLLoader.load(getClass().getResource("/app/views/login.fxml"));
+
+        }catch (IOException err){
+
+            System.out.println("FXML file not found");
+
+        }
+
+
+
+    }
+
 }
