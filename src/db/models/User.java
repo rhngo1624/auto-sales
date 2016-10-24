a5a806e93764e87e82236722089aac0961a1a908
rayrase <rmartinez2175@eagle.fgcu.edu>
2016-10-24 17:48:25 -0400

    created Users table

--- src/db/models/User.java
+++ src/db/models/User.java
@@ -1,11 +1,22 @@
 package db.models;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class User implements SQLModel {
 
     private int ID;
-    private List<Transaction> transactionIDList;
+    private String username;
+    private List<Transaction> transactions;
+    private List<HashMap<StoreItem, Integer>> cart;
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return username;
+    }
 
     public void setID(int id){
         ID = id;
