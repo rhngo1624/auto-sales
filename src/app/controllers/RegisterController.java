24bc4473495a6a4394ead3e6986b2f85b1431d82
rayrase <rmartinez2175@eagle.fgcu.edu>
2016-11-04 12:24:28 -0400

    added validate method

--- src/app/controllers/RegisterController.java
+++ src/app/controllers/RegisterController.java
@@ -2,6 +2,8 @@ package app.controllers;
 
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -49,10 +51,25 @@ public class RegisterController implements Initializable {
 
     }
 
-    private void validate(){
+    private boolean validate(){
 
+        boolean uname = usernameField.getText().isEmpty();
+        boolean fnfield = firstNameField.getText().isEmpty();
+        boolean lnfield = lastNameField.getText().isEmpty();
+        boolean addfield = addressField.getText().isEmpty();
+        boolean cityfield = cityField.getText().isEmpty();
+        boolean statefield = stateField.getText().isEmpty();
+        boolean zipfield = zipCodeField.getText().isEmpty();
+        boolean phonefield = phoneField.getText().isEmpty();
+        boolean adminfield = adminCodeField.getText().isEmpty();
 
+        boolean empty = uname && fnfield && lnfield && addfield
+                && cityfield && statefield && zipfield && phonefield
+                && adminfield;
+
+        return !(empty || !(passwordField.getText().equals(confirmPasswordField.getText())));
 
     }
+
 
 }
