4d126e37304031b6fd9e69930942cfcfc7569475
rayrase <rmartinez2175@eagle.fgcu.edu>
2016-11-07 17:14:22 -0500

    added a new attribute for accessory

--- src/db/models/Accessory.java
+++ src/db/models/Accessory.java
@@ -3,10 +3,20 @@ package db.models;
 
 public class Accessory extends StoreItem {
 
+    private String description;
+
     public Accessory(String name, double price, String imageLocation){
         setName(name);
         setPrice(price);
         setImageLocation(imageLocation);
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
     }
 
 }
