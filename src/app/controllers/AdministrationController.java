72ffbf6d504502a76962c0f0a9a8a6787b43dea0
rayrase <rmartinez2175@eagle.fgcu.edu>
2016-11-09 12:14:04 -0500

    completed users table view

--- src/app/controllers/AdministrationController.java
+++ src/app/controllers/AdministrationController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import app.ui.UsersTableView;
 import app.utils.ModalUtil;
 import app.utils.Session;
 import app.core.SQLModel;
@@ -17,13 +18,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 
 public class AdministrationController implements Initializable {
 
     @FXML
-    private ScrollPane scrollPane;
+    private BorderPane borderPane;
     @FXML
     private Label adminNameLabel;
     @FXML
@@ -61,56 +63,7 @@ public class AdministrationController implements Initializable {
 
     public void showUsers(){
 
-        GridPane userPane = new GridPane();
-
-        userPane.getColumnConstraints().add(new ColumnConstraints(50));
-        userPane.getColumnConstraints().add(new ColumnConstraints(100));
-        userPane.getColumnConstraints().add(new ColumnConstraints(100));
-        userPane.setVgap(10);
-        userPane.setHgap(10);
-        userPane.add(new Label("ID"), 0, 0);
-        userPane.add(new Label("USERNAME"), 1, 0);
-        userPane.add(new Label("ADMIN"), 2, 0);
-        userPane.setGridLinesVisible(true);
-        ObservableList<SQLModel> users = FXCollections.observableArrayList();
-        try{
-
-             users = new Users().getAllRows();
-
-        }catch(SQLException e){
-
-            ModalUtil.showWarning("Unable to load data.");
-
-        }
-
-        int rowIndex = 1;
-
-        for(SQLModel user : users){
-
-            User u = (User) user;
-
-            Label id = new Label();
-            Label name = new Label();
-            Label admin = new Label();
-
-            if(u.isAdmin()){
-                admin.setText("Admin");
-            }else{
-                admin.setText("No privileges");
-            }
-
-            id.setText(String.valueOf(u.getID()));
-            name.setText(u.getUsername());
-
-            userPane.add(id, 0, rowIndex);
-            userPane.add(name, 1, rowIndex);
-            userPane.add(admin, 2, rowIndex);
-
-            rowIndex++;
-
-        }
-
-        scrollPane.setContent(userPane);
+        borderPane.setCenter(new UsersTableView());
 
     }
 
--- src/app/ui/UsersTableView.java
+++ src/app/ui/UsersTableView.java
@@ -1,15 +1,64 @@
 package app.ui;
 
 
+import com.sun.istack.internal.Nullable;
+
+import java.sql.SQLException;
+
+import app.core.SQLModel;
 import db.models.User;
+import db.tables.Users;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class UsersTableView extends TableView<User> {
 
     public UsersTableView(){
 
-
+        setupColumns();
+        setItems(getUsers());
 
     }
+
+    private void setupColumns(){
+
+        TableColumn<User, Integer> ID = new TableColumn<>("ID");
+        TableColumn<User, String> username = new TableColumn<>("USERNAME");
+        TableColumn<User, Boolean> admin = new TableColumn<>("ADMIN PRIVILEGES");
+        ID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+        username.setCellValueFactory(new PropertyValueFactory<>("username"));
+        admin.setCellValueFactory(new PropertyValueFactory<>("isAdmin"));
+
+        ID.prefWidthProperty().bind(this.widthProperty().divide(3));
+        username.prefWidthProperty().bind(this.widthProperty().divide(3));
+        admin.prefWidthProperty().bind(this.widthProperty().divide(3));
+
+        getColumns().add(0, ID);
+        getColumns().add(1, username);
+        getColumns().add(2, admin);
+    }
+
+    private ObservableList<User> getUsers(){
+        ObservableList<User> users = FXCollections.observableArrayList();
+
+        try{
+
+            for(SQLModel u : new Users().getAllRows()){
+                User user = (User) u;
+                users.add(user);
+            }
+
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+        return users;
+
+    }
+
 
 }
