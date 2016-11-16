d57ce046b58e04c7cf3e7426ca3b4ecb4491c50b
rayrase <rmartinez2175@eagle.fgcu.edu>
2016-11-15 23:52:30 -0500

    created transaction view class

---
+++ src/app/ui/tableview/TransactionsView.java
@@ -0,0 +1,16 @@
+package app.ui.tableview;
+
+
+import javafx.scene.layout.VBox;
+
+public class TransactionsView extends VBox {
+
+    public TransactionsView(){
+
+    }
+
+    public void formTransactionView(){
+
+    }
+
+}
